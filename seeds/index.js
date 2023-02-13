7b1553af7c2f300ba79a0f335ccc3207cac10064
benmeisterz <ben.naldoza@gmail.com>
2023-02-13 09:48:33 +0800

    IRE

--- seeds/index.js
+++ seeds/index.js
@@ -3,6 +3,7 @@ const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
+mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
   useNewUrlParser: true,
   // useCreateIndex: true,
