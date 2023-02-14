c6d3799a07908ced9d7d014ab815ec2a982e7df7
benmeisterz <ben.naldoza@gmail.com>
2023-02-14 13:58:21 +0800

    logout

--- routes/users.js
+++ routes/users.js
@@ -28,27 +28,23 @@ router.get('/login', (req, res) => {
   res.render('users/login');
 })
 
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
-  req.flash('success', 'Welcome Back!');
-  // const redirectUrl = req.session.returnTo || '/campgrounds';
-  // delete req.session.returnTo;
-  res.redirect('/campgrounds');
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login', keepSessionInfo: true }), (req, res) => {
+  req.flash('success', 'welcome back!');
+  const redirectUrl = req.session.returnTo || '/campgrounds';
+  delete req.session.returnTo;
+  res.redirect(redirectUrl);
 })
 
-// router.get('/logout', (req, res) => {
-//   req.logout();
-//   req.flash('success', "Goodbye!");
-//   res.redirect('/campgrounds');
-// })
+// router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login', keepSessionInfo: true }), (req,res) => { ... }
 
-router.get('/logout', function (req, res, next) {
+router.get('/logout', (req, res, next) => {
   req.logout(function (err) {
-    if (err) {
-      return next(err);
-    }
+    if (err) { return next(err); }
+    req.flash('success', "Goodbye!");
     res.redirect('/campgrounds');
-
   });
 });
+
+
 
 module.exports = router;
