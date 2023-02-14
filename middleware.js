41776e51ab936c9ec47effb86b6468ae33407b49
benmeisterz <ben.naldoza@gmail.com>
2023-02-15 07:37:36 +0800

    isauthor

--- middleware.js
+++ middleware.js
@@ -32,6 +32,15 @@ module.exports.isAuthor = async (req, res, next) => {
   next();
 }
 
+module.exports.validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map(el => el.message).join(',')
+    throw new ExpressError(msg, 400)
+  } else {
+    next();
+  }
+}
 
 // module.exports.checkReturnTo = (req, res, next) => {
 //   if (req.session.returnTo) {
