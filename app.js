4fcab50fe04ece78ada3e62fb66387f6b1e9158e
benmeisterz <ben.naldoza@gmail.com>
2023-02-18 09:47:25 +0800

    mongo sanitize

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV !== "production") {
-  require('dotenv').config();
+    require('dotenv').config();
 }
 
 
@@ -24,17 +24,19 @@ const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
+const mongoSanitize = require('express-mongo-sanitize');
+
 mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
-  useNewUrlParser: true,
-  // useCreateIndex: true,
-  useUnifiedTopology: true
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    useUnifiedTopology: true
 });
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-  console.log("Database connected");
+    console.log("Database connected");
 });
 
 const app = express();
@@ -48,15 +50,17 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.static('public'));
 
+app.use(mongoSanitize());
+
 const sessionConfig = {
-  secret: 'thisshouldbeabettersecret!',
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    httpOnly: true,
-    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-    maxAge: 1000 * 60 * 60 * 24 * 7
-  }
+    secret: 'thisshouldbeabettersecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
 }
 app.use(session(sessionConfig))
 app.use(flash());
@@ -70,19 +74,19 @@ passport.deserializeUser(User.deserializeUser());
 
 
 app.use((req, res, next) => {
-  if (!['/login', '/'].includes(req.originalUrl)) {
-    req.session.returnTo = req.originalUrl;
-  }
-  res.locals.currentUser = req.user;
-  res.locals.success = req.flash('success');
-  res.locals.error = req.flash('error');
-  next();
+    if (!['/login', '/'].includes(req.originalUrl)) {
+        req.session.returnTo = req.originalUrl;
+    }
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
 });
 
-app.get('/fakeUser', async (req, res) => {
-  const user = new User({ email: 'squirp@gmail.com', username: "squishy" })
-  const newUser = await User.register(user, 'squish');
-  res.send(newUser);
+app.get('/fakeUser', async(req, res) => {
+    const user = new User({ email: 'squirp@gmail.com', username: "squishy" })
+    const newUser = await User.register(user, 'squish');
+    res.send(newUser);
 })
 app.use('/', userRoutes)
 app.use('/campgrounds', campgroundRoutes)
@@ -90,42 +94,40 @@ app.use('/campgrounds/:id/reviews', reviewRoutes)
 
 const validateCampground = (req, res, next) => {
 
-  const { error } = campgroundSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map(el => el.message).join(',')
-    throw new ExpressError(msg, 400)
-  } else {
-    next();
-  }
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
 }
 const validateReview = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map(el => el.message).join(',')
-    throw new ExpressError(msg, 400)
-  } else {
-    next();
-  }
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
 }
 
 app.get('/', (req, res) => {
-  res.render('home')
+    res.render('home')
 });
 
 
 
 app.all('*', (req, res, next) => {
-  next(new ExpressError('PAGE NOT FOUND', 404))
+    next(new ExpressError('PAGE NOT FOUND', 404))
 })
 
 app.use((err, req, res, next) => {
-  const { statusCode = 500 } = err;
-  if (!err.message) err.message = 'Oh No, Something Went Wrong!'
-  res.status(statusCode).render('error', { err })
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
 })
 
 app.listen(3000, () => {
-  console.log('Serving on port 3000')
+    console.log('Serving on port 3000')
 })
-
-
