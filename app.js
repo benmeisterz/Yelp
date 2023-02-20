09fb81610b908e5f39e78b2f60f963d0153942a6
benmeisterz <ben.naldoza@gmail.com>
2023-02-20 12:57:43 +0800

    session

--- app.js
+++ app.js
@@ -31,8 +31,8 @@ const reviewRoutes = require('./routes/reviews');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const MongoStore = require('connect-mongo');
-// const MongoDBStore = require("connect-mongo")(session);
-const MongoDBStore = require("connect-mongo");
+const MongoDBStore = require("connect-mongo")(session);
+// const MongoDBStore = require("connect-mongo");
 
 // const store = MongoDBStore.create({
 //     mongoUrl: dbUrl,
